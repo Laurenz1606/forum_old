2296472c99872395725a6fb2caa009505f308570
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-04 00:40:54 +0000

    add default log and url to mongodb

--- backend/src/Components/mongoose.ts
+++ backend/src/Components/mongoose.ts
@@ -1,8 +1,16 @@
 import { createConnection } from "mongoose";
+import { env } from "../Utils/env";
 import { logger } from "./logger";
 
 //create new mongoose connection
-export const mongooseConnection = createConnection("", {}, (err) => {
-  //log all errors occuring (prefere error stack else error message)
-  if (err) logger.error(err.stack ? err.stack : err.message);
-});
+export const mongooseConnection = createConnection(
+  env("MONGODB_URL", true) as string,
+  {},
+  (err) => {
+    //log all errors occuring (prefere error stack else error message)
+    if (err) return logger.error(err.stack ? err.stack : err.message);
+
+    //log create connection
+    logger.info("Mongoose: Create connection to MongoDB!");
+  },
+);
