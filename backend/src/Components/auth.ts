07c3993d159ff305a78357636804093443742977
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-05 09:49:36 +0000

    remove env test and change to refresh token

--- backend/src/Components/auth.ts
+++ backend/src/Components/auth.ts
@@ -5,8 +5,8 @@ import { logger } from "./logger";
 
 //create new auth instance
 export const auth = new AuthInstance({
-  accessTokenSecret: env("ACCESS_TOKEN_SECRET", true, ""),
-  refreshTokenSecret: env("ACCESS_TOKEN_SECRET", true),
+  accessTokenSecret: env("ACCESS_TOKEN_SECRET", true),
+  refreshTokenSecret: env("REFRESH_TOKEN_SECRET", true),
   emailValidation: true,
   expiresIn: Number(env("TOKEN_EXPIRES_IN", false, "900")),
   passwordValidation: "Y-Y-Y-Y-8",
