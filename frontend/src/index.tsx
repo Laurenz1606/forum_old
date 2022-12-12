b47da5b795bfec53cffeca455006109ac9706d02
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-12 17:28:47 +0000

    add url logic

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,42 @@
+import { setConfig } from "@authfunctions/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function init() {
+  //define default url as null
+  let url = null;
+
+  //check if in dev mode
+  if (process.env.NODE_ENV === "development") {
+    //set url to env var defined in dev mode
+    url = process.env.REACT_APP_DEV_BACKEND;
+  } else {
+    try {
+      //fetch url from /api_url route
+      const fetchedUrl = await fetch("/api_url").then((res) => res.json());
+
+      //set url to fetched url
+      url = fetchedUrl.url;
+    } catch (err) {
+      //log error
+      console.log(err);
+    }
+  }
+
+  //check if url was found
+  if (url) {
+    // set the config of authfunction
+    setConfig(url + "/api", url + "/auth");
+
+    //render react
+    const root = ReactDOM.createRoot(
+      document.getElementById("root") as HTMLElement,
+    );
+    root.render(
+      <React.StrictMode>
+      </React.StrictMode>,
+    );
+  }
+}
+
+init();
