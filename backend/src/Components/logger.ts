68ce8f9865f5b2bfff2eb7e8374788a5561f1f50
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-13 12:02:11 +0000

    update logger to enable/disable debug logs via env

--- backend/src/Components/logger.ts
+++ backend/src/Components/logger.ts
@@ -1,4 +1,8 @@
 import { Logger } from "@laurenz1606/logger";
 
 //create logger instance
-export const logger = new Logger({ format: "[%L] %t %m" });
+export const logger = new Logger({
+  format: "[%L] %t %m",
+  //enable debug logs when "DEBUG" env is set to "1" (can't use `env()` function instead of `process.env` because of import loop) 
+  debug: process.env.DEBUG === "1" ? true : false,
+});
