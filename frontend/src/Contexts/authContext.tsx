1050d1d5ae8e0b7f2bf48bc503a470abf0e78207
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-14 21:25:38 +0000

    add error messages

--- frontend/src/Contexts/authContext.tsx
+++ frontend/src/Contexts/authContext.tsx
@@ -36,7 +36,28 @@ export const authContext = createContext<AuthContextType>(
 //get auth message by auth code
 function getAuthMessage(code: number): string {
   //all error messages
-  const ERROR_MESSAGES: { [key: number]: string } = {};
+  const ERROR_MESSAGES: { [key: number]: string } = {
+    1: "The AccessToken is missing!",
+    2: "The AccessToken is invalid!",
+    5: "Their was an Error with the Server, try again later!",
+    11: "The E-Mail, the Username (handle) or the Password is missing!",
+    12: "The E-Mail has an invalid format!",
+    13: "The Password is too weak (1 uppercase letter, 1 lowercase letter, 1 number, 1 special character and at least 8 characters)!",
+    14: "The E-Mail is alredy used!",
+    15: "The Username (handle) is alredy used!",
+    21: "The E-Mail / Username (handle) or the Password is missing!",
+    22: "The User does not exist!",
+    23: "The Password is wrong!",
+    31: "The RefreshToken is missing!",
+    32: "The RefreshToken is invalid!",
+    33: "The RefreshToken does not exist!",
+    41: "The RefreshToken is missing!",
+    42: "The RefreshToken is invalid!",
+    43: "The RefreshToken does not exist!",
+    51: "The RefreshToken is missing!",
+    52: "The RefreshToken is invalid!",
+    53: "The RefreshToken does not exist!",
+  };
 
   return ERROR_MESSAGES[code] ? ERROR_MESSAGES[code] : "Unknown Error!";
 }
