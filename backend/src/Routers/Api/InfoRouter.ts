48cb218cc9ad4fb5535cb2c2baa8356a486801d1
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-14 22:21:27 +0000

    fix comment

--- backend/src/Routers/Api/InfoRouter.ts
+++ backend/src/Routers/Api/InfoRouter.ts
@@ -5,7 +5,7 @@ import { env } from "../../Utils/env";
 //the info router
 export const InfoRouter = Router();
 
-//the info methods here
+//get name of the application
 InfoRouter.get("/name", async (req, res) => {
   //send the name of the forum
   return sendData(res, 200, { name: env("FORUM_NAME", true) });
