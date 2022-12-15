9ca2e7add469e822633939f4aaec62f57cc18c44
Laurenz1606 <laurenz1606 laurenz.rausche@gmail.com>
2022-12-15 09:54:36 +0000

    test new route

--- frontend/src/Pages/DashboardPage.tsx
+++ frontend/src/Pages/DashboardPage.tsx
@@ -1,11 +1,19 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { authContext } from "../Contexts/authContext";
 
 //the dashboard page
 export default function DashboardPage() {
-  const { logout } = useContext(authContext);
+  const { logout, fetch } = useContext(authContext);
+  const [data, setData] = useState<any>({data: {data: {}}});
 
   const [error, setError] = useState<number>();
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch("/user/infos");
+      setData(data);
+    })();
+  }, []);
 
   async function onLogout() {
     const { code, err, nav } = await logout();
@@ -19,7 +27,9 @@ export default function DashboardPage() {
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>
+        Dashboard {data.data.data.fullname} ({data.data.data.username})
+      </h1>
       <button onClick={onLogout}>Logout</button>
       {error ? error : ""}
     </div>
